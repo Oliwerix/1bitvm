6ce1ff6cbb2c97b8b222fac6092a487e239a9b00
Adrian <adriansiska9@gmail.com>
2022-01-18 19:49:15 +0100

    assemblr latest chang

--- asm.py
+++ asm.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"will aseml da šit"
+"assembler for 1 bit processor"
 
 import sys
 import subprocess
@@ -10,27 +10,27 @@ def cmp(filename: str) -> bool:
     # TODO add .org
     # // predpostavljam praviln encodng fajla, pa itak je sam asci kj hces sploh pisat notr pac halo
     # Tole je naslabsa koda k sm jo kadar kol napisu -OW
-    # subprocess.run(f"nasm -e {filename} > tmp.asm".split(" "), shell=1, check=1)
-    with open("tmp.asm", "r") as infile:
-        with open(f"{''.join(filename.split('.')[:-1])}.out", "wb") as outfile:
-            for line in infile:
-                line = line.strip()
-                tokens = line.split(" ")
-                print(line)
-                if len(line) < 3 or line[0] in ["#", "/", "%"]:
-                    continue
-                if ":" in line:
-                    continue
-                com = 1 if tokens[0] == "nand" else 0
-                # tole je slabo, no sej, a je kej v temu fajlu dobr 😢
+    nasm = subprocess.run(["nasm", "-e", filename], stdout=subprocess.PIPE, text=True)
+    outs = nasm.stdout
+    with open(f"{''.join(filename.split('.')[:-1])}.out", "wb") as outfile:
+        for line in outs.splitlines():
+            line = line.strip()
+            tokens = line.split(" ")
+            print(line)
+            if len(line) < 3 or line[0] in ["#", "/", "%"]:
+                continue
+            if ":" in line:
+                continue
+            com = 1 if tokens[0] == "nand" else 0
+            # tole je slabo, no sej, a je kej v temu fajlu dobr 😢
 
-                tokens = "".join(tokens[1:]).split(",")
-                op1 = eval(tokens[0])
-                op2 = eval(tokens[1])
-                out = com % 2 << 1
-                out += op1 % 128 << 9
-                out += op2 % 128 << 2
-                outfile.write(out.to_bytes(2, "big"))
+            tokens = "".join(tokens[1:]).split(",")
+            op1 = eval(tokens[0])
+            op2 = eval(tokens[1])
+            out = com % 2 << 1
+            out += op1 % 128 << 9
+            out += op2 % 128 << 2
+            outfile.write(out.to_bytes(2, "big"))
 
 
 def main():
