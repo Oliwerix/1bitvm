f42263d0ccb80930b4da6d549887a62f5ef612b3
Adrian <adriansiska9@gmail.com>
2022-02-07 23:33:48 +0100

    Velik asm fixou  in .pylintrc dodan

--- asm.py
+++ asm.py
@@ -3,6 +3,7 @@
 
 import sys
 import subprocess
+import pdb
 
 PAGE = 2**7
 DEBUG = True
@@ -22,7 +23,7 @@ def cmp(filename: str) -> bool:
             oli_pojntr = outfile.tell()
             if int.from_bytes(outfile.read(2), "big") != 0:
                 print(
-                    f"\033[93mOverwriting prevented on {outfile.tell()}\u001b[0m",
+                    f"\033[93mOverwriting prevented on {outfile.tell():#06x}\u001b[0m",
                     file=sys.stderr,
                 )
                 return False
@@ -33,26 +34,26 @@ def cmp(filename: str) -> bool:
         print("==" * 10)
         for line in nasm.stdout.splitlines():
             line = line.strip()
-            #evaluate python scripts between graves
-            if line.count('`') % 2 == 0 and line.count('`') > 1:
-                splitline = line.split('`')
-                for i, pyMacro in enumerate(splitline[1::2]):
-                    splitline[i*2+1] = str(eval(pyMacro))
-                line = ''.join(splitline)
-                
+            if DEBUG:
+                print(line)
+            # evaluate python scripts between graves
+            if line.count("<py>") % 2 == 0 and line.count("<py>") > 1:
+                splitline = line.split("<py>")
+                for i, py_macro in enumerate(splitline[1::2]):
+                    splitline[i * 2 + 1] = str(eval(py_macro))
+                line = "".join(splitline)
             tokens = line.split(" ")
-            print(line)
             if len(line) < 3 or line[0] in ["#", "/", "%"]:
                 continue
             if ":" in line:
                 line_tokens = line.split(":")
                 poz = outfile.tell()
                 if DEBUG:
-                    print(f"{line_tokens[0]} na {hex(poz)} < {hex(lastjmp)}")
-                outfile.seek(lastjmp)
+                    print(f"{line_tokens[0]} na {poz:#06x} < {lastjmp:#06x}")
+                outfile.seek(lastjmp * 2)
                 jumps[line_tokens[0].strip()] = lastjmp
                 lastjmp += 1
-                write_safe(poz.to_bytes(2, "big"))
+                write_safe(b := poz.to_bytes(2, "big"))
                 outfile.seek(poz)
                 continue
             if ".org" in line:
@@ -70,13 +71,13 @@ def cmp(filename: str) -> bool:
             # j: typing.Callable[str, int] = lambda arg: jumps[arg]
 
             com = 1 if tokens[0] == "nand" else 0
-            tokens = "".join(tokens[1:]).split(",")
+            tokens = tuple(map(str.strip, "".join(tokens[1:]).split(",")))
             op1 = eval(tokens[0])
             op2 = eval(tokens[1])
             out = (com % 2) << 1
             # če ni nand in je podan magic bit
-            if not com and len(tokens) >= 3 and len(tok_dva := tokens[2].strip()) > 0:
-                out |= eval(tok_dva) % 2
+            if not com and len(tokens) >= 3 and len(tokens[2]) > 0:
+                out |= eval(tokens[2]) % 2
             out |= (op1 % 128) << 9
             out |= (op2 % 128) << 2
             outb: bytes = out.to_bytes(2, "big")
