77ea6660eea7baec95905b0da07bf3176d0d20eb
Adrian <adriansiska9@gmail.com>
2022-02-07 21:58:29 +0100

    Mau ga naj ne bi pokadil!

--- asm.py
+++ asm.py
@@ -3,47 +3,49 @@
 
 import sys
 import subprocess
-import typing
 
 PAGE = 2**7
+DEBUG = True
 
 
 def cmp(filename: str) -> bool:
     "compile"
-    nasm = subprocess.run(["nasm", "-e", filename], stdout=subprocess.PIPE, text=True)
-    outs = nasm.stdout
+    nasm = subprocess.run(
+        ["nasm", "-e", filename], stdout=subprocess.PIPE, text=True, check=True
+    )
     lastjmp = 1
-    jumps: dict[str, int] = dict()
+    jumps: dict[str, int] = {}
     with open(f"{''.join(filename.split('.')[:-1])}.out", "w+b") as outfile:
 
-        def writeSafe(out: bytes) -> bool:
+        def write_safe(out: bytes) -> bool:
             "writes int to current position in file, also checks for possible overwriting"
-            Oli_pojntr = outfile.tell()
+            oli_pojntr = outfile.tell()
             if int.from_bytes(outfile.read(2), "big") != 0:
                 print(
                     f"\033[93mOverwriting prevented on {outfile.tell()}\u001b[0m",
                     file=sys.stderr,
                 )
                 return False
-            outfile.seek(Oli_pojntr)
+            outfile.seek(oli_pojntr)
             outfile.write(out)
             return True
 
         print("==" * 10)
-        for line in outs.splitlines():
+        for line in nasm.stdout.splitlines():
             line = line.strip()
             tokens = line.split(" ")
             print(line)
             if len(line) < 3 or line[0] in ["#", "/", "%"]:
                 continue
             if ":" in line:
-                ln = line.split(":")
+                line_tokens = line.split(":")
                 poz = outfile.tell()
-                print(f"{ln[0]} na {hex(poz)} < {hex(lastjmp)}")
+                if DEBUG:
+                    print(f"{line_tokens[0]} na {hex(poz)} < {hex(lastjmp)}")
                 outfile.seek(lastjmp)
-                jumps[ln[0].strip()] = lastjmp
+                jumps[line_tokens[0].strip()] = lastjmp
                 lastjmp += 1
-                writeSafe(poz.to_bytes(2, "big"))
+                write_safe(poz.to_bytes(2, "big"))
                 outfile.seek(poz)
                 continue
             if ".org" in line:
@@ -51,28 +53,27 @@ def cmp(filename: str) -> bool:
                 outfile.seek(org, 0)
                 continue
             if ".db" in line:
-
-                db: bytes = eval(line.split(".db")[1].strip())
-                for gg in (db[i : i + 1] for i in range(0, len(db), 4)):
-                    writeSafe(gg)
-                if len(db) % 2 != 0:
-                    writeSafe(bytes((db[len(db) - 1], 0)))
+                data_bin: bytes = eval(line.removeprefix(".db ").strip())
+                for part in (data_bin[i : i + 1] for i in range(0, len(data_bin), 4)):
+                    write_safe(part)
+                if len(data_bin) % 2 != 0:
+                    write_safe(bytes((data_bin[len(data_bin) - 1], 0)))
                 continue
 
-            def j(arg: str) -> int:
-                return jumps[arg]
+            # j: typing.Callable[str, int] = lambda arg: jumps[arg]
 
             com = 1 if tokens[0] == "nand" else 0
             tokens = "".join(tokens[1:]).split(",")
             op1 = eval(tokens[0])
             op2 = eval(tokens[1])
             out = (com % 2) << 1
-            if not com and len(tokens) >= 3:
-                out |= eval(tokens[2]) % 2
+            # če ni nand in je podan magic bit
+            if not com and len(tokens) >= 3 and len(tok_dva := tokens[2].strip()) > 0:
+                out |= eval(tok_dva) % 2
             out |= (op1 % 128) << 9
             out |= (op2 % 128) << 2
             outb: bytes = out.to_bytes(2, "big")
-            if not writeSafe(outb):
+            if not write_safe(outb):
                 return False
         print("==" * 10)
         return True
--- example.asm
+++ example.asm
@@ -5,8 +5,8 @@ word:
 .db b"Bazo "
 
 main:
-    print2 jumps["word"]  , 0x15
-    print2 jumps["word"]+2, 0x15
+    print2 `jumps["word"]  `, 0x15
+    print2 `jumps["word"]+2`, 0x15
 
 .org 0x0
 c j("main"), 0, 1
