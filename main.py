51be465b1f9dee6644eb03e3f948d7bfee8fe90c
Adrian Šiška <adriansiska9@gmail.com>
2022-03-17 09:16:08 +0100

    Pač mičkn estetike, brisane neuporabljene kode

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 "main project file?"
 
 import sys
-import IPython
+
+# import IPython
 
 import oneb_vm
 
@@ -18,12 +19,12 @@ def main():
         if opt.startswith("-") and "d" in opt:
             global DEBUG
             DEBUG = True
-        if opt.startswith("-") and "i" in opt:
-            IPython.embed()
+        # if opt.startswith("-") and "i" in opt:
+        # IPython.embed()
     if DEBUG:
         virtual_machine.run(210, lambda x: x.dump_state())
     else:
-        virtual_machine.run(8000)
+        virtual_machine.run(-1)
 
 
 if __name__ == "__main__":
